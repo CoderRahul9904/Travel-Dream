cfd1d1666f2d6b5d4ea3c9aba8d2a429dcd2c0e4
CoderRahul9904 <mouryarahul543@gmail.com>
2024-07-03 00:11:07 +0530

    done with user model

---
+++ backend/models/UserModels.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose')
+
+const UserSchema= new mongoose.Schema({
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    phoneNumber: String,
+    address: String,
+    dateOfBirth: Date,
+    role: { type: String, enum: ['Admin', 'Customer'], default: 'Customer' },
+    bookingHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Booking' }],
+})
+
+
+const User=mongoose.model('UserInfo',UserSchema,'UserInfo')
+
+module.exports=User
