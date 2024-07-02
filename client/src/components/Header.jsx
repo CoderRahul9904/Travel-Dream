2d5f8f092164947457d2fed18fbda8419612123d
CoderRahul9904 <mouryarahul543@gmail.com>
2024-07-02 15:34:30 +0530

    done with header, heroContainer

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,18 @@ import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Header = ({ className = "" }) => {
+const Header = ({ className = "", bgColor,textColor }) => {
+
+  if( bgColor== " "){
+    bgColor="transparent"
+  }else{
+    bgColor="bg-white h-[77px]"
+  }
   const navigate = useNavigate();
+
+  const handleProfileNavigation =useCallback(()=>{
+    navigate("/profile");
+  }, [navigate])
 
   const onFickleflightLogoContainerClick = useCallback(() => {
     navigate("/");
@@ -15,7 +25,7 @@ const Header = ({ className = "" }) => {
 
   return (
     <header
-      className={`self-stretch bg-white h-[77px] flex flex-row items-center justify-center py-[22px] px-20 box-border sticky w-full top-[0] [background:white] text-left text-sm text-darkslategray-200 font-roboto lg:pl-10 lg:pr-10 lg:box-border md:pl-6 md:pr-6 md:box-border ${className}`}
+      className={`self-stretch  flex flex-row ${bgColor} items-center justify-center py-[22px] px-20 box-border sticky w-full top-[0] text-left text-sm text-darkslategray-200 font-roboto lg:pl-10 lg:pr-10 lg:box-border md:pl-6 md:pr-6 md:box-border ${className}`}
     >
       <div className="flex flex-row items-center justify-between">
         <div
@@ -33,40 +43,43 @@ const Header = ({ className = "" }) => {
         </div>
         <div className="flex flex-row items-center justify-end gap-[36px] sm:flex">
           <div className="flex flex-row items-center justify-end gap-[30px] md:hidden">
-            <div
-              className="relative cursor-pointer"
+            <div 
+              className= {` relative cursor-pointer text-${textColor}`}
               onClick={onFickleflightLogoContainerClick}
             >
               Explore
             </div>
             <div
-              className="relative cursor-pointer"
+              className= {` relative cursor-pointer text-${textColor}`}
               onClick={onSearchTextClick}
             >
               Search
             </div>
-            <button className="cursor-pointer [border:none] p-0 bg-[transparent] relative text-sm font-roboto text-cornflowerblue-200 text-left inline-block">
+            <button className={ `cursor-pointer [border:none] p-0 bg-[transparent] relative text-${textColor} text-sm font-roboto text-cornflowerblue-200 text  text-left inline-block`}>
               Hotels
             </button>
-            <button className="cursor-pointer [border:none] p-0 bg-[transparent] relative text-sm font-roboto text-darkslategray-200 text-left inline-block">
+            <button className={`cursor-pointer [border:none] p-0 bg-[transparent] relative text-sm font-roboto text-${textColor} text-darkslategray-200 text-left inline-block`}>
               Offers
             </button>
           </div>
           <div className="flex flex-row items-center justify-center gap-[12px]">
             <img
-              className="relative w-6 h-6 overflow-hidden hidden md:flex"
-              alt=""
-              src="/notification.svg"
-            />
-            <img
               className="relative w-6 h-6 overflow-hidden"
               alt=""
               src="/notification1.svg"
             />
+            <button onClick={handleProfileNavigation} className=" bg-transparent cursor-pointer">
             <img
               className="rounded-19xl w-9 h-9 object-cover"
               alt=""
               src="/top_avatar@2x.png"
+            />
+            </button>
+            
+            <img
+              className="relative w-6 h-6 overflow-hidden hidden md:flex"
+              alt=""
+              src="/notification.svg"
             />
           </div>
         </div>
--- client/src/components/HeroContainer.jsx
+++ client/src/components/HeroContainer.jsx
@@ -3,72 +3,21 @@ import { Radio, FormControlLabel } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import FormInputsRowContainer from "./FormInputsRowContainer";
 import PropTypes from "prop-types";
+import Header from "./Header";
 
 const HeroContainer = ({ className = "" }) => {
   const navigate = useNavigate();
 
+
   const onSearchFlightsButtonClick = useCallback(() => {
     navigate("/results-page");
-  }, [navigate]);
-
-  const onHotelsTextClick = useCallback(() => {
-    navigate("/hotels-page");
   }, [navigate]);
 
   return (
     <div
       className={`self-stretch flex flex-col items-center justify-start text-center text-42xl text-white font-baloo-bhai ${className}`}
     >
-      <header className="self-stretch bg-white h-[77px] flex flex-row items-center justify-center py-[22px] px-20 box-border sticky w-full top-[0] [background:white] text-left text-sm text-darkslategray-200 font-roboto lg:pl-10 lg:pr-10 lg:box-border md:pl-6 md:pr-6 md:box-border">
-        <div className="flex flex-row items-center justify-between">
-          <button className="cursor-pointer [border:none] p-0 bg-[transparent] relative w-[174px] h-[33px] overflow-hidden">
-            <img
-              className="absolute h-[96.97%] w-[99.83%] top-[1.59%] right-[0.17%] bottom-[1.44%] left-[0%] max-w-full overflow-hidden max-h-full"
-              alt=""
-              src="/symbols.svg"
-            />
-          </button>
-          <div className="flex flex-row items-center justify-end gap-[36px] sm:flex">
-            <div className="flex flex-row items-center justify-end gap-[30px] md:hidden">
-              <button className="cursor-pointer [border:none] p-0 bg-[transparent] relative text-sm font-roboto text-cornflowerblue-200 text-left inline-block">
-                Explore
-              </button>
-              <div
-                className="relative cursor-pointer"
-                onClick={onSearchFlightsButtonClick}
-              >
-                Search
-              </div>
-              <div
-                className="relative cursor-pointer"
-                onClick={onHotelsTextClick}
-              >
-                Hotels
-              </div>
-              <button className="cursor-pointer [border:none] p-0 bg-[transparent] relative text-sm font-roboto text-darkslategray-200 text-left inline-block">
-                Offers
-              </button>
-            </div>
-            <div className="flex flex-row items-center justify-center gap-[12px]">
-              <img
-                className="relative w-6 h-6 overflow-hidden hidden md:flex"
-                alt=""
-                src="/notification.svg"
-              />
-              <img
-                className="relative w-6 h-6 overflow-hidden"
-                alt=""
-                src="/notification1.svg"
-              />
-              <img
-                className="rounded-19xl w-9 h-9 object-cover"
-                alt=""
-                src="/top_avatar@2x.png"
-              />
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
       <div className="self-stretch relative h-[640px]">
         <div className="absolute w-full top-[0px] right-[0px] left-[0px] [background:linear-gradient(78.78deg,_#114f8b_5.2%,_#2a9ad7)] h-[639.9px]" />
         <img
