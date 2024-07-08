ea2806d3c7dda13bbcfd4bf1bad71d0b3c9171d0
CoderRahul9904 <mouryarahul543@gmail.com>
2024-07-08 23:51:56 +0530

    working

--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -1,6 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit"
 import userReducer from "../slices/UserSlice"
 
+
+console.log("working")
 const store = configureStore({
     reducer: {
       user: userReducer,
