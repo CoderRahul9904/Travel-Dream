f505eddfce9b7610f9ec9fd456b8d2d6cf1629b9
CoderRahul9904 <mouryarahul543@gmail.com>
2024-07-08 23:53:03 +0530

    working

--- client/src/components/authDropDown.jsx
+++ client/src/components/authDropDown.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import { useDispatch, useSelector } from "react-redux";
+
 import { login, logout } from '../slices/UserSlice';
 
 function AuthGoogle() {
@@ -10,10 +11,28 @@ function AuthGoogle() {
 
   const googleId = '124526521008-qbc8uhv8i9kun6ciq2db6pgc2sdfujps.apps.googleusercontent.com';
 
-  const handleGoogleLogin = (response) => {
+  const handleGoogleLogin = async (response) => {
     console.log('Login Success:', response);
     const token = response.credential;
     localStorage.setItem('googleToken', token);
+    try {
+      
+      const userInfoResponse = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+        headers: {
+          'Authorization': `Bearer ${response.access_token}`
+        }
+      });
+
+      if (!userInfoResponse.ok) {
+        throw new Error('Failed to fetch user profile');
+      }
+
+      const data = await userInfoResponse.json();
+      console.log('Profile Data:', data);
+      
+    } catch (error) {
+      console.error('Error fetching user profile:', error);
+    }
     dispatch(login({ googleToken: token }));
   };
 
@@ -27,6 +46,7 @@ function AuthGoogle() {
         onSuccess={handleGoogleLogin}
         onError={handleUnsuccessfulGoogleLogin}
         useOneTap={false}
+        scope="https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
       />
     </GoogleOAuthProvider>
   );
