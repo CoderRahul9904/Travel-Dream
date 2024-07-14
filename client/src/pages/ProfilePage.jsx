e3fceaf2fa74e20d9d80461410bce7bfd6ac3c2b
CoderRahul9904 <mouryarahul543@gmail.com>
2024-07-14 23:53:36 +0530

    done

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -12,16 +12,16 @@ import AuthGoogle from '../components/authDropDown';
 function ProfilePage() {
     const dispatch = useDispatch();
     const navigate=useNavigate()
-    const { isLoggedIn, googleToken } = useSelector((state) =>state.user)
+    const { isLoggedIn, googleToken, userInfo } = useSelector((state) =>state.user)
+
     const handleLogout = () => {
         googleLogout()
-        localStorage.removeItem('googleToken')
+        localStorage.removeItem('token')
         dispatch(logout());
         console.log('Logged Out')
         navigate('/')
 
     };
-    const token=isLoggedIn
   return (
     <div className=' relative w-screen h-[500px] '>
         <img 
@@ -34,18 +34,17 @@ function ProfilePage() {
             <Header bgColor=" " textColor="white" />
         </div>
         {/* Profile Header */}
-        {token? <div className=' '>
+        {isLoggedIn && userInfo ? <div className=' '>
         <div className=' absolute top-[100px] left-[15%] w-[70%] sm:left-0 flex justify-evenly items-center rounded-3xl h-auto sm:w-full bg-stone-100'>
-            <div className='w-[200px] bg-cornflowerblue-500 my-16 h-[200px] flex flex-col justify-start items-center rounded-full sm:w-[100px] sm:h-[100px] sm:rounded-[100px] overflow-hidden md:w-[150px] md:h-[150px] md:rounded-[150px]'>
-                {/* <img className=' w-full h-full object-fill' src={proImg} alt='Profile Image'/> */}
-                <p className='pt-5 md:pt-0 text-13xl md:text-5xl text-white sm:text-lg'>RM</p>
-                <button className=' cursor-pointer bg-cornflowerblue-100 text-sm text-stone-200'>Add Profile Photo</button>
+            <div className=' relative w-[180px] bg-cornflowerblue-500 my-16 h-[180px] flex flex-col justify-start items-center rounded-full sm:w-[100px] sm:h-[100px] sm:rounded-[100px] overflow-hidden md:w-[150px] md:h-[150px] md:rounded-[150px]'>
+                <img className=' text-13xl md:text-5xl text-white sm:text-lg w-full h-full object-cover' src={userInfo.picture} alt='Profile Image'></img>
+                <button className=' absolute bottom-6 rounded sm:hidden cursor-pointer bg-stone-600 text-sm text-stone-200'>Add Profile Photo</button>
             </div>
             <div className=' mx-10 flex flex-col gap-0'>
                 <p className='  font-sans font-bold text-19xl text-stone-900'>
-                    Rahul Mourya
+                    {userInfo.name}
                 </p>
-                <p className=' text-sm font-light text-stone-600'>mouryarahul543@gmail.com</p>
+                <p className=' text-sm font-light text-stone-600'>{userInfo.email}</p>
             </div>
         </div>
 
@@ -63,19 +62,19 @@ function ProfilePage() {
               <tbody>
               <tr className='border-b'>
                 <td className='text-lg text-stone-500 font-serif py-2 w-1/3'>Mobile Number</td>
-                <td className='text-lg text-stone-500 py-2'>+91-7262022151</td>
+                <td className='text-lg text-stone-500 py-2'>-</td>
              </tr>
             <tr className='border-b'>
                 <td className='text-lg text-stone-500 font-serif sm:hidden py-2'>Email Address</td>
-                <td className='text-lg text-stone-500 sm:hidden py-2'>mouryarahul543@gmail.com</td>
+                <td className='text-lg text-stone-500 sm:hidden py-2'>{userInfo.email}</td>
             </tr>
             <tr className='border-b'>
                 <td className='text-lg text-stone-500 font-serif py-2'>First Name</td>
-                <td className='text-lg text-stone-500 py-2'>Rahul</td>
+                <td className='text-lg text-stone-500 py-2'>{userInfo.given_name}</td>
             </tr>
             <tr className='border-b'>
                 <td className='text-lg text-stone-500 font-serif py-2'>Last Name</td>
-                <td className='text-lg text-stone-500 py-2'>Mourya</td>
+                <td className='text-lg text-stone-500 py-2'>{userInfo.family_name}</td>
             </tr>
             <tr className='border-b'>
                 <td className='text-lg text-stone-500 font-serif py-2'>Gender</td>
