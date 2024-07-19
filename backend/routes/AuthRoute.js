5190a560478d8181125235127f5817cce3c5989e
CoderRahul9904 <mouryarahul543@gmail.com>
2024-07-20 00:08:52 +0530

    working

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -2,7 +2,7 @@ const express=require('express')
 const AuthRoute=express.Router()
 const AuthVerification=require('../controllers/authController')
 
-
+console.log("working")
 AuthRoute
     .route('/get-info')
     .post(AuthVerification.Auth_Verification)
